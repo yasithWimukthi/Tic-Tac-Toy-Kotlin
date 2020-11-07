583ddfd980c432473d2f216daf1ef07c8e3132b0
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-07 19:35:50 +0530

    Create buClick function

--- app/src/main/java/com/androidstudio/kotlin/tictactoygame/MainActivity.kt
+++ app/src/main/java/com/androidstudio/kotlin/tictactoygame/MainActivity.kt
@@ -2,10 +2,29 @@ package com.androidstudio.kotlin.tictactoygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun buClick(view:View){
+        val buSelected = view as Button
+        var cellID = 0
+
+        when(buSelected.id){
+            R.id.bu1 -> cellID=1
+            R.id.bu2 -> cellID=2
+            R.id.bu3 -> cellID=3
+            R.id.bu4 -> cellID=4
+            R.id.bu5 -> cellID=5
+            R.id.bu6 -> cellID=6
+            R.id.bu7 -> cellID=7
+            R.id.bu8 -> cellID=8
+            R.id.bu9 -> cellID=9
+        }
     }
 }
